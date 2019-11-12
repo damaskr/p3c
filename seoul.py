f21bc83229d947d302c614eedfa77aaa68af2ab0
damaskr <damaskr@gmail.com>
2019-11-12 23:37:19 +0900

    goodjob

--- seoul.py
+++ seoul.py
@@ -6,22 +6,23 @@ def fuck(pagenum):
 
     result = [[],[],[],[]]
 
-    url = 'http://www.sen.go.kr/web/services/bbs/bbsList.action?bbsBean.bbsCd=119&searchBean.searchKey=&appYn=&searchBean.searchVal=&searchBean.startDt=&startDt=&searchBean.endDt=&endDt=&ctgCd=&sex=&school=&grade=&year=&month=&schoolDiv=&establDiv=&hopearea=&searchBean.deptCd=&searchBean.currentPage=1'+str(pagenum)
+    url = 'http://www.sen.go.kr/web/services/bbs/bbsList.action?bbsBean.bbsCd=119&searchBean.searchKey=&appYn=&searchBean.searchVal=&searchBean.startDt=&startDt=&searchBean.endDt=&endDt=&ctgCd=&sex=&school=&grade=&year=&month=&schoolDiv=&establDiv=&hopearea=&searchBean.deptCd=&searchBean.currentPage='+str(pagenum)
     r = requests.get(url)
     html = r.text
     soup  = bs(html, 'html.parser')
     tr = soup.select('tbody > tr')
     for i in range(len(tr)):
-        if tr[i].select('td')[4].text.strip():
-            result[0].append(tr[i].select('td')[1].text.strip())
-            result[1].append(tr[i].select('td')[2].text.strip())
-            result[2].append(tr[i].select('td')[4].text.strip())
+        result[0].append(tr[i].select('td')[1].text.strip())
+        result[1].append(tr[i].select('td')[2].text.strip())
+        result[2].append(tr[i].select('td')[4].text.strip())
 
     for i in range(len(result[0])):
-        if "ddd" in result[2][i]
-            print(result[0][i], result[1][i], result[2][i]))
-
+        if "방과후" in result[2][i]:
+            print('%8s' % result[0][i],'%5s' % result[1][i], '%15s' % result[2][i])
 
 fuck(1)
-fuck(2)
-fuck(3)
+fuck(11)
+fuck(21)
+fuck(31)
+fuck(41)
+fuck(51)
