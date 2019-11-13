7daddddef7250a8d0d71371d32116813153729bf
damaskr <damaskr@gmail.com>
2019-11-13 11:17:42 +0900

    address add

--- seoul.py
+++ seoul.py
@@ -2,10 +2,10 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 
-def fuck(pagenum):
+def seoul(pagenum):
 
-    result = [[],[],[],[]]
-
+    result = [[],[],[],[],[]]
+    refurl = 'http://www.sen.go.kr'
     url = 'http://www.sen.go.kr/web/services/bbs/bbsList.action?bbsBean.bbsCd=119&searchBean.searchKey=&appYn=&searchBean.searchVal=&searchBean.startDt=&startDt=&searchBean.endDt=&endDt=&ctgCd=&sex=&school=&grade=&year=&month=&schoolDiv=&establDiv=&hopearea=&searchBean.deptCd=&searchBean.currentPage='+str(pagenum)
     r = requests.get(url)
     html = r.text
@@ -15,14 +15,16 @@ def fuck(pagenum):
         result[0].append(tr[i].select('td')[1].text.strip())
         result[1].append(tr[i].select('td')[2].text.strip())
         result[2].append(tr[i].select('td')[4].text.strip())
-
+    href = soup.select('tbody> tr > td > a')
+    for i in range(len(href)):
+        result[3].append(refurl+href[i].get('href'))
     for i in range(len(result[0])):
         if "방과후" in result[2][i]:
-            print('%8s' % result[0][i],'%5s' % result[1][i], '%15s' % result[2][i])
+            print(result[0][i],result[1][i], result[2][i], result[3][i])
 
-fuck(1)
-fuck(11)
-fuck(21)
-fuck(31)
-fuck(41)
-fuck(51)
+seoul(1)
+seoul(11)
+seoul(21)
+seoul(31)
+seoul(41)
+seoul(51)
